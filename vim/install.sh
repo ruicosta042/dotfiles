f6d0f0e2be6ad830d7df95f0d3ec37e194339c54
Rui Costa <ruicosta042@gmail.com>
2019-12-15 22:49:36 +0000

    [vim] nord color scheme

--- vim/install.sh
+++ vim/install.sh
@@ -2,10 +2,29 @@
 
 mac () {
   brew install vim
+
+  setup
 }
 
 ubuntu () {
   sudo apt-get install vim -y
+
+  setup
+}
+
+setup () {
+  cp vim/_vimrc "$HOME/.dotfiles/vimrc"
+  ln -sf "$HOME/.dotfiles/vimrc" "$HOME/.vimrc"
+
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+  vim +PlugInstall +qall
+
+  cat << EOT >> "$HOME/.dotfiles/vimrc"
+
+colorscheme nord
+EOT
 }
 
 os_call "[vim] install?" mac ubuntu
