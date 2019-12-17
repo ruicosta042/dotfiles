b5794d7ddc87b78ba136af406dd20455331a04e1
Rui Costa <ruicosta042@gmail.com>
2019-12-17 21:50:52 +0000

    [hosts] use new shell

--- hosts/install.sh
+++ hosts/install.sh
@@ -9,10 +9,12 @@ ubuntu () {
 }
 
 setup () {
-  git clone https://github.com/StevenBlack/hosts "hosts/hosts"
-  cd "hosts/hosts" || return 1;
-  pip3 install --user -r requirements.txt
-  python3 updateHostsFile.py --auto --replace --backup --flush-dns-cache --compress
+  (
+    git clone https://github.com/StevenBlack/hosts "hosts/hosts"
+    cd "hosts/hosts" || return 1;
+    pip3 install --user -r requirements.txt
+    python3 updateHostsFile.py --auto --replace --backup --flush-dns-cache --compress
+  )
 }
 
 os_call "[hosts] install?" mac ubuntu
