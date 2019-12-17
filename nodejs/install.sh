23c58397ffa45b4c9b815de11aba73593a433bbc
Rui Costa <ruicosta042@gmail.com>
2019-12-17 21:18:36 +0000

    [nodejs] fix install

--- nodejs/install.sh
+++ nodejs/install.sh
@@ -1,50 +1,29 @@
 #!/usr/bin/env bash
 
-mac_nvm () {
+install() {
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
-  setup_nvm
-}
-
-ubuntu_nvm () {
-  wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
-  setup_nvm
-}
-
-setup_nvm () {
-  # add nvm to startup apps
-  cat << EOT >> "bash/bash_start"
-
-# nvm
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \\. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \\. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-EOT
 
   # load nvm without exiting this shell
-  export NVM_DIR="${XDG_CONFIG_HOME/:-$HOME/.}nvm"
+  NVM_DIR="$(realpath ~/.nvm)"
+  export NVM_DIR
   [ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
-}
 
-mac () {
-  mac_nvm
-  setup
-}
-
-ubuntu () {
-  ubuntu_nvm
-  setup
-}
-
-setup () {
-  source "$HOME/.nvm/nvm.sh"
   nvm install node
 
   # allow calling local packages' binaries without prefix
-  cat << EOT >> "bash/bash_profile"
+  cat << EOT >> "$DOTFILES_DIR/bash_profile"
 
 # Node.js: allow calling local packages' binaries without prefix
 export PATH=\$PATH:./node_modules/.bin
 EOT
+}
+
+mac () {
+  install
+}
+
+ubuntu () {
+  install
 }
 
 os_call "[nodejs] install?" mac ubuntu
