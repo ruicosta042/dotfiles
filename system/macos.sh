6570a1bc69a4de577835cce79e866d79d6ab1ed7
Rui Costa <ruicosta042@gmail.com>
2021-04-23 10:35:55 +0100

    fix: adjust macos key repeat settings

--- system/macos.sh
+++ system/macos.sh
@@ -35,8 +35,8 @@ defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
 
 # Set a fast keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 6
-defaults write NSGlobalDomain InitialKeyRepeat -int 15
+defaults write NSGlobalDomain KeyRepeat -int 12
+defaults write NSGlobalDomain InitialKeyRepeat -int 25
 
 # Disable the “Are you sure you want to open this application?” dialog
 defaults write com.apple.LaunchServices LSQuarantine -bool false
